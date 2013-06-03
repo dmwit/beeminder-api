1a958eee4399bd9eab05568f4b942276dd4bf007
Brent Yorgey <byorgey@cis.upenn.edu>
2013-06-02 20:29:15 -0400

    untabify, and export Beeminder type

--- Network/Beeminder.hs
+++ Network/Beeminder.hs
@@ -1,31 +1,32 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses, TypeFamilies #-}
 module Network.Beeminder
-	-- TODO: this export list is hopelessly incomplete
-	( UserGoals(..)
-	, User(..)
-	, Point(..)
-	, Goal(..)
-	, Token
-	-- UserGoals stuff
-	, Burner(..), LevelOfGoalDetail(..)
-	-- Goal stuff
-	, TimeFrame(..), Aggregate(..), Direction(..), Behavior(..), Target(..), GoalType(..)
-	, HasID(..), HasUpdatedAt(..), HasName(..), HasTimezone(..), HasUsername(..), HasGoals(..), HasGoalsFilter(..), HasLevelOfDetail(..)
-	, HasPointCount(..), HasTimestamp(..), HasValue(..), HasComment(..), HasRequestID(..), HasGoal(..), HasPointRequest(..), HasPointRequests(..)
-	, HasGetPoints(..), HasTitle(..), HasType(..), HasTarget(..), HasBehavior(..), HasPanic(..)
-	, now
-	, user
-	, goal, allGoals
-	, createGoal
-	, points
-	, createPoint , createPointNotify
-	, createPoints, createPointsNotify
-	, updatePoint , deletePoint
-	, gType
-	, runBeeminder
-	-- TODO: don't export this
-	, externalize
-	) where
+        -- TODO: this export list is hopelessly incomplete
+        ( Beeminder
+        , UserGoals(..)
+        , User(..)
+        , Point(..)
+        , Goal(..)
+        , Token
+        -- UserGoals stuff
+        , Burner(..), LevelOfGoalDetail(..)
+        -- Goal stuff
+        , TimeFrame(..), Aggregate(..), Direction(..), Behavior(..), Target(..), GoalType(..)
+        , HasID(..), HasUpdatedAt(..), HasName(..), HasTimezone(..), HasUsername(..), HasGoals(..), HasGoalsFilter(..), HasLevelOfDetail(..)
+        , HasPointCount(..), HasTimestamp(..), HasValue(..), HasComment(..), HasRequestID(..), HasGoal(..), HasPointRequest(..), HasPointRequests(..)
+        , HasGetPoints(..), HasTitle(..), HasType(..), HasTarget(..), HasBehavior(..), HasPanic(..)
+        , now
+        , user
+        , goal, allGoals
+        , createGoal
+        , points
+        , createPoint , createPointNotify
+        , createPoints, createPointsNotify
+        , updatePoint , deletePoint
+        , gType
+        , runBeeminder
+        -- TODO: don't export this
+        , externalize
+        ) where
 
 import Control.Applicative
 import Control.Monad.Base
@@ -40,13 +41,13 @@ import Network.HTTP.Conduit
 import qualified Network.Beeminder.Internal as Internal
 
 data BeeminderEnvironment = BeeminderEnvironment
-	{ token   :: Token
-	, manager :: Manager
-	}
+        { token   :: Token
+        , manager :: Manager
+        }
 
 type Beeminder_ = MaybeT (ReaderT BeeminderEnvironment (ResourceT IO))
 newtype Beeminder a = Beeminder { unBeeminder :: Beeminder_ a }
-	deriving (Functor, Applicative, Monad, MonadIO, MonadReader BeeminderEnvironment, MonadThrow, MonadUnsafeIO, MonadResource, MonadBase IO)
+        deriving (Functor, Applicative, Monad, MonadIO, MonadReader BeeminderEnvironment, MonadThrow, MonadUnsafeIO, MonadResource, MonadBase IO)
 
 -- The following instance (and the "deriving" clause for MonadThrow,
 -- MonadUnsafeIO, MonadResource, and MonadBase IO) were copied basically
@@ -54,20 +55,20 @@ newtype Beeminder a = Beeminder { unBeeminder :: Beeminder_ a }
 -- typechecking" rather than with some deep understanding of what's happening.
 -- So it wouldn't surprise me if there's bugs here.
 instance MonadBaseControl IO Beeminder where
-	data StM Beeminder a = StM !(StM Beeminder_ a)
-	liftBaseWith f = Beeminder (liftBaseWith (\g -> f (\(Beeminder m) -> StM <$> g m)))
-	restoreM (StM v) = Beeminder (restoreM v)
+        data StM Beeminder a = StM !(StM Beeminder_ a)
+        liftBaseWith f = Beeminder (liftBaseWith (\g -> f (\(Beeminder m) -> StM <$> g m)))
+        restoreM (StM v) = Beeminder (restoreM v)
 
 runBeeminder :: Token -> Beeminder a -> IO (Maybe a)
 runBeeminder t m = do
-	man <- newManager def
-	runResourceT (runReaderT (runMaybeT (unBeeminder m)) BeeminderEnvironment { token = t, manager = man })
+        man <- newManager def
+        runResourceT (runReaderT (runMaybeT (unBeeminder m)) BeeminderEnvironment { token = t, manager = man })
 
 externalize :: FromJSON a => (Token -> params -> Request Beeminder) -> params -> Beeminder a
 externalize f p = do
-	BeeminderEnvironment { token = t, manager = m } <- ask
-	r <- httpLbs (f t p) {responseTimeout = Nothing} m
-	Beeminder . MaybeT . return . decode . responseBody $ r
+        BeeminderEnvironment { token = t, manager = m } <- ask
+        r <- httpLbs (f t p) {responseTimeout = Nothing} m
+        Beeminder . MaybeT . return . decode . responseBody $ r
 
 user        :: UserParameters        -> Beeminder User
 goal        :: GoalParameters        -> Beeminder Goal
